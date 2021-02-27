f3b103b819552856c9f684ee5723950b53519910
RolphMc <rmc.rodolfo@gmail.com>
2021-02-26 22:36:27 -0300

    update: exercício

--- exercise.js
+++ exercise.js
@@ -2,37 +2,35 @@
 function form_validate_hour(h) {
     var bg = window.document.getElementById('introduction')
     var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('img')
+    var img = window.document.getElementById('img-container')
     var data = new Date()
     var hora = data.getHours()
     
+    msg.innerHTML = `Lembre-se, agora são ${hora} horas`
+
     if(document.getElementById("fstart").value != ""){
-        var hora = document.getElementById("fstart");
+        var hora = window.document.getElementById("fstart");
     }
-    
-    
-    /*var hora = 22
-    msg.innerHTML = `Agora são ${hora} horas`*/
+
     if (h || document.getElementById("fstart").value != ""){
         switch (true) {
-            case (04 > hora && hora >= 00):
-                img.src = 'img/exercise-sample1/00hours.png'
+            case (4 > hora && hora >= 0):
+                img.innerHTML = `<img src="00hours.png">` 
                 break;
-            case (06 > hora && hora >= 04):
-                img.src = 'img/exercise-sample1/04hours.png'
+            case (6 > hora && hora >= 4):
+                img.innerHTML = `<img src="04hours.png">` 
                 break;
-            case (12 > hora && hora >= 06):
-                img.src = 'img/exercise-sample1/06hours.png'
+            case (12 > hora && hora >= 6):
+                img.innerHTML = `<img src="06hours.png">` 
                 break;
             case (17 > hora && hora >= 12):
-                img.src = 'img/exercise-sample1/12hours.png'
+                img.innerHTML = `<img src="12hours.png">` 
                 break;
             case (20 > hora && hora >= 17):
-                img.src = "img/exercise-sample1/17hours.png"
+                img.innerHTML = `<img src="17hours.png">` 
                 break;
             case (23 > hora && hora >= 20):
-                img.src = 'img/exercise-sample1/20hours.png'
-                document.body.style.background = 'linear-gradient(to top, #375790, #96D6F2)'
+                img.innerHTML = `<img src="20hours.png">` 
                 break;
         }
     }
